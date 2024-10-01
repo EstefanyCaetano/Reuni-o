0e6ae329ef74fa0d933c87d9e021f9da395c86e5
Estefany Caetano <estefanycaetano@outlook.com>
2024-10-01 20:40:02 -0300

    Update index.js

--- Web/index.js
+++ Web/index.js
@@ -1,9 +1,1 @@
-//function calculo() {
-//   let horai = document.getElementById("horai")
-//    let horaf = document.getElementById("horaf")
-//    let duracao = document.getElementById("span")
-    
-//    duracao = parseInt(horai.value) + parseInt(horaf.value)
-//    console.log("A Duração foi:", duracao);
 
-//}
